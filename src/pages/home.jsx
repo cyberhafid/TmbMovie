878c1ead84c0c74b080d812f8fad0e68e0963e26
BENAMAR Hafid <cyberhafid@gmail.com>
2019-05-03 21:41:04 +0200

    mise en place compansants

---
+++ src/pages/home.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'reactstrap';
+import Premiere './pages/Premiere';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Carousel1 />
+        <div className="container-fluid backg">
+         
+          <Row>
+            <Col sm="3">
+              <p>col1</p>
+            </Col>
+            <Col sm="6">
+      <Premiere />
+            </Col>
+            <Col sm="3">
+            <p>col3</p>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    );
+  }
+}
